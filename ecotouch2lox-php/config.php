457ac59491f3aee575b093399116815c91ea715a
schlickspringer <9342605+schlickspringer@users.noreply.github.com>
2017-08-22 19:23:15 +0200

    updates

--- ecotouch2lox-php/config.php
+++ ecotouch2lox-php/config.php
@@ -13,14 +13,14 @@ define(LOXONE_USER, ''); // Loxone username
 define(LOXONE_PWD, ''); // Loxone password
 
 // specify the virtual input for each ecotouch tag you're interested in
-$lox_vi['temperature_outside'] = "VI3";
-$lox_vi['temperature_outside_1h'] = "VI4";
-$lox_vi['temperature_outside_24h'] = "VI5";
-$lox_vi['temperature_source_in'] = "VI6";
-$lox_vi['temperature_source_out'] = "VI7";
-$lox_vi['temperature_evaporation'] = "VI8";
-$lox_vi['temperature_return_set'] = "VI9";
-$lox_vi['temperature_return'] = "VI10";
-$lox_vi['temperature_flow'] = "VI11";
+$lox_vi['TEMPERATURE_OUTSIDE'] = "VI3";
+$lox_vi['TEMPERATURE_OUTSIDE_1H'] = "VI4";
+$lox_vi['TEMPERATURE_OUTSIDE_24H'] = "VI5";
+$lox_vi['TEMPERATURE_SOURCE_IN'] = "VI6";
+$lox_vi['TEMPERATURE_SOURCE_OUT'] = "VI7";
+$lox_vi['TEMPERATURE_EVAPORATION'] = "VI8";
+$lox_vi['TEMPERATURE_RETURN_SET'] = "VI9";
+$lox_vi['TEMPERATURE_RETURN'] = "VI10";
+$lox_vi['TEMPERATURE_FLOW'] = "VI11";
 
 ?>
